7425515ceb0c43f0e2469d156750584a51785fdb
knapptan <nikitas@live.ru>
2025-02-07 15:25:24 +0300

    Настроил сборку и добавил кор дата модель

---
+++ Task+CoreDataProperties.swift
@@ -0,0 +1,28 @@
+//
+//  Task+CoreDataProperties.swift
+//  T2_ToDoListApp
+//
+//  Created by Knapptan on 07.02.2025.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Task {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Task> {
+        return NSFetchRequest<Task>(entityName: "Task")
+    }
+
+    @NSManaged public var title: String?
+    @NSManaged public var creationDate: Date?
+    @NSManaged public var taskDescription: String?
+    @NSManaged public var isCompleted: Bool
+
+}
+
+extension Task : Identifiable {
+
+}
